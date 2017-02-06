030bd7838c841842c826524c8d6caf99eab6525c
Damien Le Berrigaud <damien@itsdamo.com>
2017-02-06 15:06:07 -0700

    Map servlet.

--- src/main/java/org/superbiz/moviefun/ActionServlet.java
+++ src/main/java/org/superbiz/moviefun/ActionServlet.java
@@ -17,8 +17,10 @@
 package org.superbiz.moviefun;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,29 +28,37 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * @version $Revision$ $Date$
- */
+@Component
 public class ActionServlet extends HttpServlet {
 
     private static final long serialVersionUID = -5832176047021911038L;
+    private static final int PAGE_SIZE = 5;
 
-    public static int PAGE_SIZE = 5;
+    private final MoviesBean moviesBean;
+    private final PlatformTransactionManager transactionManager;
 
-    @EJB
-    private MoviesBean moviesBean;
+    public ActionServlet(MoviesBean moviesBean, PlatformTransactionManager transactionManager) {
+        this.moviesBean = moviesBean;
+        this.transactionManager = transactionManager;
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TransactionStatus transaction = transactionManager.getTransaction(null);
+        processWithoutTransaction(request, response);
+        transactionManager.commit(transaction);
+    }
+
+    private void processWithoutTransaction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
 
         if ("Add".equals(action)) {
